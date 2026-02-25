763945779c2e74c2a6e8a1423f65892767f90c3d
james-perry785 <james.perry@hmcts.net>
2026-02-25 17:00:38 +0000

    add to smoke test

--- src/test/functional/features/hello-world-second.feature
+++
@@ -1,6 +0,0 @@
-Feature: Example feature file
-
-    Scenario: Example scenario
-        When I go to '/'\
-        And I wait for 1 second
-        Then the page goes to 'https://example.com'
--- src/test/smoke/smoke.ts
+++ src/test/smoke/smoke.ts
@@ -1,23 +1,51 @@
-import { fail } from 'assert';
-
+import { beforeAll, describe, expect, test } from '@jest/globals';
 import axios, { AxiosResponse } from 'axios';
-import { expect } from 'chai';
 
 const testUrl = process.env.TEST_URL || 'http://localhost:3100';
 
 describe('Smoke Test', () => {
   describe('Home page loads', () => {
-    test('with correct content', async () => {
+    let response: AxiosResponse;
+
+    beforeAll(async () => {
       try {
-        const response: AxiosResponse = await axios.get(testUrl, {
+        response = await axios.get(testUrl, {
           headers: {
             'Accept-Encoding': 'gzip',
           },
         });
-        expect(response.data).includes('<h1 class="govuk-heading-xl">Default page template</h1>');
-      } catch {
-        fail('Heading not present and/or correct');
+      } catch (error: unknown) {
+        console.error(`Failed to fetch ${testUrl}:`);
+        if (axios.isAxiosError(error)) {
+          console.error(`Error message: ${error.message}`);
+          if (error.code) {
+            console.error(`Axios error code: ${error.code}`); // e.g., ECONNREFUSED, ETIMEDOUT
+          }
+          if (error.response) {
+            console.error(`Server responded with status ${error.response.status}:`, error.response.data);
+          }
+        } else if (error instanceof Error) {
+          console.error(`Error message: ${error.message}`);
+        } else {
+          console.error('An unexpected error occurred:', error);
+        }
+        throw error;
       }
+    });
+
+    test('with correct content', () => {
+      expect(response.data).toContain('<h1 class="govuk-heading-xl">Default page template</h1>');
+    });
+
+    test('with correct header', () => {
+      expect(response.data).toContain('<div class="govuk-header__logo">');
+    });
+
+    test('with correct footer and copyright', () => {
+      expect(response.data).toContain('All content is available under the');
+      expect(response.data).toMatch(
+        /<a\s*class="govuk-footer__link"\s*href=".*?"\s*rel="license"\s*>Open Government Licence v3.0<\/a>/
+      );
     });
   });
 });
