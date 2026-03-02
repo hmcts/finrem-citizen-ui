9548f107404471551543a8b64b3849af4ef7e1d4
james-perry785 <james.perry@hmcts.net>
2026-03-02 13:45:33 +0000

    updated playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,16 +4,17 @@ import { defineBddConfig } from 'playwright-bdd';
 // 1. Determine the target URL
 const testUrl = process.env.TEST_URL || 'http://localhost:3100';
 
-const bddDir = defineBddConfig({
+defineBddConfig({
   paths: ['src/test/functional/features/*.feature'],
   require: ['src/test/steps/*.steps.ts', 'src/test/fixtures/fixtures.ts'],
+  importTestFrom: 'src/test/fixtures/fixtures.ts',
 });
 
 export default defineConfig({
   testDir: './',
   testMatch: [
-    bddDir + '/**/*.spec.js', // include BDD tests
-    '**/src/test/a11y/*.test.ts', // include AXE tests
+    '.features-gen/**/*.spec.js', // include BDD tests
+    'src/test/a11y/*.test.ts', // include AXE tests
   ],
 
   reporter: [['html'], ['json', { outputFile: 'test-results.json' }], ['allure-playwright']],
