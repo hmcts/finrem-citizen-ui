84318cab03d10e1c99e431306c2b54d3912ec191
james-perry785 <james.perry@hmcts.net>
2026-03-03 12:50:59 +0000

    updated reporting naming in pipeline for visibility

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,9 +8,9 @@ export default defineConfig({
   testMatch: ['a11y/*.test.ts', 'functional/**/*.spec.ts'],
 
   reporter: [
-    ['html', { outputFolder: 'functional-output/html' }],
+    ['html', { outputFolder: 'functional-output/report', open: 'never' }],
     ['allure-playwright', { resultsDir: 'allure-results' }],
-    ['junit', { outputFile: 'functional-output/junit.xml' }],
+    ['junit', { outputFile: 'functional-output/functional-results.xml' }],
   ],
   timeout: 30 * 1000,
   expect: {
