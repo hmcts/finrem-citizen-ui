54a3b335b413ee55f40378878015f8d0d2f21f9c
james-perry785 <james.perry@hmcts.net>
2026-03-04 11:40:14 +0000

    Revert "update smoke test to be just jest, remove chai"
    This reverts commit ae28a245bfd2e3bb62f4b6f3247452d7b4994b87.

--- src/test/smoke/smoke.ts
+++ src/test/smoke/smoke.ts
@@ -1,15 +1,23 @@
-import { describe, expect, test } from '@jest/globals';
+import { fail } from 'assert';
+
 import axios, { AxiosResponse } from 'axios';
+import { expect } from 'chai';
 
 const testUrl = process.env.TEST_URL || 'http://localhost:3100';
 
 describe('Smoke Test', () => {
-  test('Home page loads with correct content', async () => {
-    const response: AxiosResponse = await axios.get(testUrl, {
-      headers: { 'Accept-Encoding': 'gzip' },
+  describe('Home page loads', () => {
+    test('with correct content', async () => {
+      try {
+        const response: AxiosResponse = await axios.get(testUrl, {
+          headers: {
+            'Accept-Encoding': 'gzip',
+          },
+        });
+        expect(response.data).includes('<h1 class="govuk-heading-xl">Default page template</h1>');
+      } catch {
+        fail('Heading not present and/or correct');
+      }
     });
-
-    expect(response.status).toBe(200);
-    expect(response.data).toContain('<h1 class="govuk-heading-xl">Default page template</h1>');
   });
 });
