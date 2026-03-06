6b1eb976ad4e61f4ea21aca876d3ead46177db00
james-perry785 <james.perry@hmcts.net>
2026-03-06 16:37:32 +0000

    updated jest config

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,9 @@
 module.exports = {
   roots: ['<rootDir>/src/test/unit'],
+  // Only run unit tests by default
+  // Ignore node_modules and a11y tests by default, as they are run separately
+  testPathIgnorePatterns: ['/node_modules/', '<rootDir>/src/test/a11y/'],
   testRegex: '(\\.(test|spec))\\.(ts|js)$',
-  moduleFileExtensions: ['ts', 'js', 'json'],
   testEnvironment: 'node',
   transform: {
     '^.+\\.ts?$': 'ts-jest',
