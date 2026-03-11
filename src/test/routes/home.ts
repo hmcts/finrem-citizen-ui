1f1b2607e93b2c34bf20690bc201dbe567f3e62e
Matt Searle <mgsearle@proton.me>
2026-03-11 14:30:47 +0000

    fix failing tests

--- src/test/routes/home.ts
+++ src/test/routes/home.ts
@@ -4,11 +4,11 @@ import { app } from '../../main/app';
 
 describe('Home page', () => {
   describe('on GET', () => {
-    test('should redirect to enter case number page', async () => {
+    test('should redirect to login when not authenticated', async () => {
       const res = await request(app).get('/').expect(302);
 
       expect(res.status).toBe(302);
-      expect(res.header.location).toBe('/enter-case-number');
+      expect(res.header.location).toBe('/login');
     });
   });
 });
