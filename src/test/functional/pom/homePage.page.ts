a2beb8ad75d5ede4e6359189e5c608f1a5e7511a
philipafrane21 <philip.afrane@solirius.com>
2026-03-12 17:36:33 +0000

    Updated header logo locator to use getByRole

--- src/test/functional/pom/homePage.page.ts
+++ src/test/functional/pom/homePage.page.ts
@@ -9,7 +9,7 @@ export class HomePage {
 
   constructor(private readonly page: Page) {
     this.heading = this.page.locator('h1.govuk-heading-xl');
-    this.headerLogo = this.page.locator('svg.govuk-header__logotype');
+    this.headerLogo = this.page.getByRole('img', { name: 'GOV.UK' });
     this.footer = this.page.locator('footer');
     this.licenceLink = this.page.locator('a.govuk-footer__link[rel="license"]');
   }
