5dc9b5cf12e56cd32f4041a6a5e0a3a8d25d0193
udaydenduluri33 <Uday.Denduluri1@HMCTS.NET>
2026-03-16 10:57:13 +0000

    rollback cve

--- src/main/app/auth/service/get-service-auth-token.ts
+++ src/main/app/auth/service/get-service-auth-token.ts
@@ -1,6 +1,6 @@
-import { authenticator } from '@otplib/v12-adapter';
 import axios from 'axios';
 import config from 'config';
+import { authenticator } from 'otplib';
 
 const { Logger } = require('@hmcts/nodejs-logging');
 
@@ -10,20 +10,16 @@ let token: string;
 export const getTokenFromApi = (): void => {
   logger.info('Refreshing service auth token');
 
-  const url: string = `${config.get<string>('services.authProvider.url')}/lease`;
-  const microservice: string = config.get<string>('services.authProvider.microservice');
-  const secret: string = config.get<string>('services.authProvider.secret');
+  const url: string = config.get('services.authProvider.url') + '/lease';
+  const microservice: string = config.get('services.authProvider.microservice');
+  const secret: string = config.get('services.authProvider.secret');
   const oneTimePassword = authenticator.generate(secret);
   const body = { microservice, oneTimePassword };
 
   axios
     .post(url, body)
-    .then(response => {
-      token = response.data;
-    })
-    .catch(err => {
-      logger.error(err.response?.status, err.response?.data);
-    });
+    .then(response => (token = response.data))
+    .catch(err => logger.error(err.response?.status, err.response?.data));
 };
 
 export const initAuthToken = (): void => {
