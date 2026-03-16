654fc0f7223b1125813c8f44dfbb877f6cad8e18
gergelykiss <gergely.kiss@solirius.com>
2026-03-16 10:18:54 +0000

    redis infra update

--- src/main/app/auth/service/get-service-auth-token.ts
+++ src/main/app/auth/service/get-service-auth-token.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import config from 'config';
-import { authenticator } from 'otplib';
+import { authenticator } from '@otplib/v12-adapter';
 
 const { Logger } = require('@hmcts/nodejs-logging');
 
@@ -10,16 +10,20 @@ let token: string;
 export const getTokenFromApi = (): void => {
   logger.info('Refreshing service auth token');
 
-  const url: string = config.get('services.authProvider.url') + '/lease';
-  const microservice: string = config.get('services.authProvider.microservice');
-  const secret: string = config.get('services.authProvider.secret');
+  const url: string = `${config.get<string>('services.authProvider.url')}/lease`;
+  const microservice: string = config.get<string>('services.authProvider.microservice');
+  const secret: string = config.get<string>('services.authProvider.secret');
   const oneTimePassword = authenticator.generate(secret);
   const body = { microservice, oneTimePassword };
 
   axios
     .post(url, body)
-    .then(response => (token = response.data))
-    .catch(err => logger.error(err.response?.status, err.response?.data));
+    .then(response => {
+      token = response.data;
+    })
+    .catch(err => {
+      logger.error(err.response?.status, err.response?.data);
+    });
 };
 
 export const initAuthToken = (): void => {
