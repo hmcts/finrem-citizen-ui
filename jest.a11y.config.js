048251258ccb184ec3f0ceb98534a391e8f877a3
james-perry785 <james.perry@hmcts.net>
2026-03-16 16:34:35 +0000

    add robust route-based accessibility scanning using jest and Pa11y

--- jest.a11y.config.js
+++ jest.a11y.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  roots: ['<rootDir>/src/test/a11y'],
+  roots: ['<rootDir>/src/test/jest-a11y'],
   testRegex: '(/src/test/.*|\\.(test|spec))\\.(ts|js)$',
   moduleFileExtensions: ['ts', 'js', 'json'],
   testEnvironment: 'node',
