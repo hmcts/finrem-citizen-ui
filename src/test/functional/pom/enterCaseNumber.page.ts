50cbb54ee83b3ac17e456fcd18633a9cee5c961a
philipafrane21 <philip.afrane@solirius.com>
2026-03-16 11:59:16 +0000

    Added enterCaseNumber POM with constructor and locators

---
+++ src/test/functional/pom/enterCaseNumber.page.ts
@@ -0,0 +1,21 @@
+import { Locator, Page } from '@playwright/test';
+
+export class EnterCaseNumberPage {
+  readonly caseNumberInput: Locator;
+  readonly caseNumberLabel: Locator;
+  readonly caseNumberHint: Locator;
+  readonly continueBtn: Locator;
+
+  constructor(readonly page: Page) {
+    this.caseNumberInput = page.getByLabel('Case number')
+    this.caseNumberLabel = this.page.locator('label[for="caseNumber"]');
+    this.caseNumberHint = this.page.locator('#caseNumber-hint');
+    this.continueBtn = this.page.getByRole('button', { name: 'Continue' });
+  }
+
+    async enterCaseNumber(caseNumber: string): Promise<void> {
+    await this.caseNumberInput.fill(caseNumber);
+    await this.continueBtn.click();
+  }
+
+}
