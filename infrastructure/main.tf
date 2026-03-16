a57e230a46c44c05720316d8a0f4cc2f261fec3a
udaydenduluri33 <Uday.Denduluri1@HMCTS.NET>
2026-03-16 11:46:05 +0000

    using variable for resource group

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -8,7 +8,7 @@ locals {
 
 data "azurerm_key_vault" "finrem_key_vault" {
   name                = local.azureVaultName
-  resource_group_name = var.key_vault_resource_group
+  resource_group_name = local.azureVaultName
 }
 
 module "redis-cache-v2" {
