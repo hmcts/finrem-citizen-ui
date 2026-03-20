e8f1f940953eb4c8322e30047fd9d3e48dabb2a5
udaydenduluri33 <Uday.Denduluri1@HMCTS.NET>
2026-03-20 11:26:28 +0000

    split public and private routes

--- src/main/route-names.ts
+++ src/main/route-names.ts
@@ -1,15 +1,22 @@
-export const RouteNames = {
+export const PublicRoutes = {
   basePath: '/',
   callbackUrl: '/oauth2/callback',
+  health: '/health',
+  info: '/info',
+  login: '/login',
+  logout: '/logout',
+} as const;
+
+export const PrivateRoutes = {
   caseUserRole: '/case/:caseReference/:userId/:caseRole',
   caseReference: '/case/:caseReference',
   enterCaseNumber: '/enter-case-number',
   dashboard: '/dashboard',
   enterAccessCode: '/enter-access-code',
-  health: '/health',
-  info: '/info',
-  login: '/login',
-  logout: '/logout',
-  taskListUpload: '/task-list-upload-dashboard'
+  taskListUpload: '/task-list-upload-dashboard',
 } as const;
 
+export const RouteNames = {
+  ...PublicRoutes,
+  ...PrivateRoutes,
+} as const;
