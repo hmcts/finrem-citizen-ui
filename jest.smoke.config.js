292ced124855ec7a150dbf15f169a96fd5e47c3a
james-perry785 <james.perry@hmcts.net>
2026-03-26 09:39:01 +0000

    add picomatch to package json

--- jest.smoke.config.js
+++ jest.smoke.config.js
@@ -31,7 +31,7 @@ module.exports = {
       'jest-html-reporter',
       {
         pageTitle: 'Smoke Test Report',
-        outputPath: '<rootDir>/smoke-output/reports/test-report.html',
+        outputPath: '<rootDir>/smoke-output/reports/smoke-test-report.html',
         includeFailureMsg: true,
       },
     ],
