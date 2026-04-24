310c3eb8dcda6a53236e08ab03a5972645114c81
Matt Searle <mgsearle@proton.me>
2026-04-24 09:48:02 +0100

    Add @azure to Jest transformIgnorePatterns to fix ESM module transformation

--- jest.config.js
+++ jest.config.js
@@ -13,7 +13,7 @@ module.exports = {
       },
     ],
   },
-  transformIgnorePatterns: ['/node_modules/(?!(openid-client|oauth4webapi|jose|otplib|@otplib|@scure|@noble|uuid)/)'],
+  transformIgnorePatterns: ['/node_modules/(?!(openid-client|oauth4webapi|jose|otplib|@otplib|@scure|@noble|uuid|@azure)/)'],
   setupFilesAfterEnv: ['<rootDir>/src/test/jest.setup.ts'],
   collectCoverageFrom: [
     'src/main/**/*.ts',
