5ae4ef7377830f13d3df016c0431d56f78ebdd9e
Matt Searle <mgsearle@proton.me>
2026-04-24 09:28:54 +0100

    Add uuid to Jest transformIgnorePatterns to fix ESM module transformation

--- jest.config.js
+++ jest.config.js
@@ -13,7 +13,7 @@ module.exports = {
       },
     ],
   },
-  transformIgnorePatterns: ['/node_modules/(?!(openid-client|oauth4webapi|jose|otplib|@otplib|@scure|@noble)/)'],
+  transformIgnorePatterns: ['/node_modules/(?!(openid-client|oauth4webapi|jose|otplib|@otplib|@scure|@noble|uuid)/)'],
   setupFilesAfterEnv: ['<rootDir>/src/test/jest.setup.ts'],
   collectCoverageFrom: [
     'src/main/**/*.ts',
