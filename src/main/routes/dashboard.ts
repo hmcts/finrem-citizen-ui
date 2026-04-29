eeceaca730b9298d3a241aa532d5cb9f2cdb71dd
Matt Searle <mgsearle@proton.me>
2026-04-29 11:58:52 +0100

    refactor getting help section into macro, add initial dashboard layout

--- src/main/routes/dashboard.ts
+++ src/main/routes/dashboard.ts
@@ -5,6 +5,10 @@ import { oidcMiddleware } from '../middleware';
 
 export default function setupDashboardRoute(app: Application): void {
   app.get(RouteNames.dashboard, oidcMiddleware, (req: Request, res: Response) => {
-    res.render(ViewNames.Dashboard);
+    res.render(ViewNames.Dashboard, {
+      userName: 'Sam Thompson',
+      caseNumber: '1674-2354-3687-4537',
+      hasDivorceCase: true,
+    });
   });
 }
