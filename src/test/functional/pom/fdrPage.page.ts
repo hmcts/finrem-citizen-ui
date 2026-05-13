30b008f8a843938329f14dec5ebf41b025d98948
james-perry785 <james.perry@hmcts.net>
2026-05-13 11:17:47 +0100

    add pom for fdr page

---
+++ src/test/functional/pom/fdrPage.page.ts
@@ -0,0 +1,100 @@
+import { expect, Locator, Page } from '@playwright/test';
+
+import { BasePage } from './basePage.page';
+import { GettingHelpPanel } from './components/gettingHelpPanel.component';
+
+const URL_PATTERNS = {
+  DASHBOARD: /\/dashboard/,
+  DOCUMENT_SELECTION: /\/upload\/document-selection/,
+  FDR: /\/upload\/fdr/,
+};
+
+// FDR = Financial Dispute Resolution.
+const FDR_EMAIL = 'FRCexample@justice.gov.uk';
+
+export class FdrPage extends BasePage {
+  readonly backLink: Locator;
+  readonly questionHeading: Locator;
+  readonly hintText: Locator;
+  readonly yesOption: Locator;
+  readonly noOption: Locator;
+  readonly continueButton: Locator;
+  readonly cancelLink: Locator;
+  readonly inlineErrorMessage: Locator;
+  readonly gettingHelp: GettingHelpPanel;
+
+  constructor(readonly page: Page) {
+    super(page);
+    this.backLink = this.page.getByRole('link', { name: 'Back' });
+    this.questionHeading = this.page.getByRole('heading', {
+      name: 'Are you uploading these documents for a Financial Dispute Resolution hearing?',
+    });
+    this.hintText = this.page.getByText(
+      'This should be written on the hearing notice you received from the court.',
+      { exact: true }
+    );
+    this.yesOption = this.page.getByLabel('Yes');
+    this.noOption = this.page.getByLabel('No, they are for a different hearing');
+    this.continueButton = this.page.getByRole('button', { name: 'Continue' });
+    this.cancelLink = this.page.getByRole('link', { name: 'Cancel' });
+    this.inlineErrorMessage = this.page.getByText(
+      /Error:\s*Select yes if you are uploading these documents for a Financial Dispute Resolution hearing/i
+    );
+    this.gettingHelp = new GettingHelpPanel(this.page);
+  }
+
+  async verifyFdrPageContent(): Promise<void> {
+    await expect(this.page).toHaveURL(URL_PATTERNS.FDR);
+    await this.verifyGlobalHeaderAndFooter();
+
+    await this.expectVisible([
+      this.serviceNav,
+      this.navigationLink,
+      this.signOutBtn,
+      this.backLink,
+      this.questionHeading,
+      this.hintText,
+      this.yesOption,
+      this.noOption,
+      this.continueButton,
+      this.cancelLink,
+      this.gettingHelp.heading,
+      this.gettingHelp.summary,
+    ]);
+
+    await this.expectAttributes([
+      { locator: this.backLink, name: 'href', value: '/upload/confidentiality' },
+      { locator: this.cancelLink, name: 'href', value: '/dashboard' },
+    ]);
+  }
+
+  async submitWithoutSelectionAndExpectValidationError(): Promise<void> {
+    await this.continueButton.click();
+    await this.expectVisible([this.inlineErrorMessage]);
+  }
+
+  async selectYesAndContinue(): Promise<void> {
+    await this.yesOption.check();
+    await this.continueButton.click();
+    await expect(this.page).toHaveURL(URL_PATTERNS.DOCUMENT_SELECTION);
+  }
+
+  async selectNoAndContinue(): Promise<void> {
+    await this.noOption.check();
+    await this.continueButton.click();
+    await expect(this.page).toHaveURL(URL_PATTERNS.DOCUMENT_SELECTION);
+  }
+
+  async clickCancelAndExpectDashboard(): Promise<void> {
+    await this.cancelLink.click();
+    await expect(this.page).toHaveURL(URL_PATTERNS.DASHBOARD);
+  }
+
+  async verifyContactHelpClosedByDefault(): Promise<void> {
+    await this.gettingHelp.verifyClosedByDefault();
+  }
+
+  async verifyContactHelpContent(): Promise<void> {
+    await this.gettingHelp.verifyContactContent({ expectedEmail: FDR_EMAIL });
+  }
+}
