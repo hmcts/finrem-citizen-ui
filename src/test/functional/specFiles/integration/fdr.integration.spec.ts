ae66845d455dad86ef43bcfb6426fd7fc2e28ada
james-perry785 <james.perry@hmcts.net>
2026-05-26 12:14:47 +0100

    remove document-selection scenarios from FDR integration test suite

--- src/test/functional/specFiles/integration/fdr.integration.spec.ts
+++ src/test/functional/specFiles/integration/fdr.integration.spec.ts
@@ -7,7 +7,7 @@ import { navigateToFdrStep } from '../journeyHelpers/uploadJourneyNavigation.hel
  *
  * FDR = Financial Dispute Resolution. This suite verifies the real upload
  * journey behavior for the FDR question page, including content, validation,
- * navigation, and progression to document selection.
+ * navigation, and getting-help support.
  *
  * Setup:
  * - Uses authenticated `loggedInPage` fixture (real IDAM login flow)
@@ -50,87 +50,6 @@ test.describe('[integration] FDR page', () => {
   }) => {
     await fdrPage.selectYesAndContinue();
     await documentSelectionPage.verifyDocumentSelectionPageContent();
-    await runA11yAudit(axeUtils);
-  });
-
-  test('[integration] Document selection supports adding a document @a11y', async ({
-    fdrPage,
-    documentSelectionPage,
-    axeUtils,
-  }) => {
-    await fdrPage.selectYesAndContinue();
-    await documentSelectionPage.addDocumentBySearchTerm('payslip', 'Payslips');
-    await documentSelectionPage.expectDocumentsListContains(['Payslips']);
-    await runA11yAudit(axeUtils);
-  });
-
-  test('[integration] Document selection supports adding multiple documents @a11y', async ({
-    fdrPage,
-    documentSelectionPage,
-    axeUtils,
-  }) => {
-    await fdrPage.selectYesAndContinue();
-    await documentSelectionPage.addDocumentBySearchTerm('payslip', 'Payslips');
-    await documentSelectionPage.addDocumentBySearchTerm('bank', 'Bank statements');
-    await documentSelectionPage.expectDocumentsListContains(['Payslips', 'Bank statements']);
-    await runA11yAudit(axeUtils);
-  });
-
-  test('[integration] Document selection supports removing a document @a11y', async ({
-    fdrPage,
-    documentSelectionPage,
-    axeUtils,
-  }) => {
-    await fdrPage.selectYesAndContinue();
-    await documentSelectionPage.addDocumentBySearchTerm('payslip', 'Payslips');
-    await documentSelectionPage.addDocumentBySearchTerm('bank', 'Bank statements');
-
-    await documentSelectionPage.removeDocumentByLabel('Payslips');
-    await documentSelectionPage.expectDocumentsListContains(['Bank statements']);
-    await runA11yAudit(axeUtils);
-  });
-
-  test('[integration] Document selection requires at least one document before continuing @a11y', async ({
-    fdrPage,
-    documentSelectionPage,
-    axeUtils,
-  }) => {
-    await fdrPage.selectYesAndContinue();
-    await documentSelectionPage.submitWithoutDocumentsAndExpectValidationError();
-    await runA11yAudit(axeUtils);
-  });
-
-  test('[integration] Document selection continue navigates to upload documents after adding documents @a11y', async ({
-    fdrPage,
-    documentSelectionPage,
-    axeUtils,
-  }) => {
-    await fdrPage.selectYesAndContinue();
-    await documentSelectionPage.addDocumentBySearchTerm('payslip', 'Payslips');
-    await documentSelectionPage.clickContinueAndExpectUploadDocumentsStep();
-    await runA11yAudit(axeUtils);
-  });
-
-  test('[integration] Document selection getting help panel shows expected contact details @a11y', async ({
-    fdrPage,
-    documentSelectionPage,
-    axeUtils,
-  }) => {
-    await fdrPage.selectYesAndContinue();
-    await documentSelectionPage.verifyGettingHelpSection();
-    await runA11yAudit(axeUtils);
-  });
-
-  test('[integration] Document selection back navigation returns to FDR and retains selected documents @a11y', async ({
-    fdrPage,
-    documentSelectionPage,
-    axeUtils,
-  }) => {
-    await fdrPage.selectYesAndContinue();
-    await documentSelectionPage.addDocumentBySearchTerm('payslip', 'Payslips');
-
-    await documentSelectionPage.clickBackToFdrAndReturnWithYesSelection();
-    await documentSelectionPage.expectDocumentsListContains(['Payslips']);
     await runA11yAudit(axeUtils);
   });
 
