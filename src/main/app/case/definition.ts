e1864ff92aaccc256de7fca7c86cb96f971d6bcb
Uday Denduluri <Uday.Denduluri2@hmcts.net>
2026-06-19 09:26:47 +0100

    Revert "Set isFDR when document is uploaded (#484)" (#490)
    This reverts commit 018a5cfed82632aed44edbc8cc8ef9ab7313296c.

--- src/main/app/case/definition.ts
+++ src/main/app/case/definition.ts
@@ -3045,7 +3045,6 @@ export interface FeeItem {
 export interface CitizenUploadDocument extends HasCaseDocument {
     generalDocumentUploadDateTime: DateAsString;
     DocumentType: CitizenUploadDocumentType;
-    isFDR: YesOrNo;
     DocumentEmailContent: string;
     DocumentLink: CitizenDocument;
     DocumentDateAdded: DateAsString;
