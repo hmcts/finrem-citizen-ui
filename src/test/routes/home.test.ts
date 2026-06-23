2c82a6a42e71d739a9465b0e9fcf0d362045119c
james-perry785 <james.perry@hmcts.net>
2026-06-23 09:34:46 +0100

    mock AppInsights for error telemetry verification in home route

--- src/test/routes/home.test.ts
+++ src/test/routes/home.test.ts
@@ -1,8 +1,15 @@
+import { describe, expect, jest, test } from '@jest/globals';
 import request from 'supertest';
 
 import { app } from '../../main/app';
 
+jest.mock('../../main/modules/appinsights');
+
 describe('Home page', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
   describe('on GET', () => {
     test('should redirect to login when not authenticated', async () => {
       const res = await request(app).get('/').expect(302);
