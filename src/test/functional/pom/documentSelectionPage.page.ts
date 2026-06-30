2350c1c0385be881f3f6a3165c2a1a311ae94d45
Matt Searle <mgsearle@proton.me>
2026-06-30 12:20:57 +0100

    fix: update functional test URL patterns to match renamed route document-type-selection and update confirmation page text

--- src/test/functional/pom/documentSelectionPage.page.ts
+++ src/test/functional/pom/documentSelectionPage.page.ts
@@ -4,7 +4,7 @@ import { BasePage } from './basePage.page';
 import { GETTING_HELP_OPENING_HOURS, GettingHelpPanel } from './components/gettingHelpPanel.component';
 
 const URL_PATTERNS = {
-  DOCUMENT_SELECTION: /\/upload\/document-selection/,
+  DOCUMENT_SELECTION: /\/upload\/document-type-selection/,
   FDR: /\/upload\/fdr/,
   UPLOAD_DOCUMENTS: /\/upload\/upload-documents/,
 };
