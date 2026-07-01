9b6669d456f417ecb7caab0aa66c92010a94028d
james-perry785 <james.perry@hmcts.net>
2026-07-01 15:07:27 +0100

    add page load wait to DocumentUploadPage.clickContinue

--- src/test/functional/pom/documentUploadPage.page.ts
+++ src/test/functional/pom/documentUploadPage.page.ts
@@ -259,6 +259,8 @@ export class DocumentUploadPage extends BasePage {
 
   async clickContinue(): Promise<void> {
     await this.continueButton.click();
+    await this.page.waitForURL(/\/upload\/check-upload/, { timeout: 15000 });
+    await this.page.waitForLoadState('networkidle', { timeout: 10000 });
   }
 
   async clickBack(): Promise<void> {
