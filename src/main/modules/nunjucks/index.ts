049a35debd208cf8997a78d7a5076cca8c54c6ca
Uday Denduluri <Uday.Denduluri2@hmcts.net>
2026-07-03 14:50:23 +0100

    DFR-5122 - adding smart survey link (#506)
    * adding smart survey link
    
    * Update beta banner feedback link to use regex matching and validate SmartSurvey URL
    
    ---------
    
    Co-authored-by: Matt Searle <mgsearle@proton.me>

--- src/main/modules/nunjucks/index.ts
+++ src/main/modules/nunjucks/index.ts
@@ -7,11 +7,37 @@ import { taskListFormItems } from '../../functions/task-list/task-list-form-item
 import { taskListWarningMessage } from '../../functions/task-list/task-list-warning-message';
 import { taskStatus } from '../../functions/task-list/task-status';
 
+const FEEDBACK_SURVEY_BASE_URL = 'https://www.smartsurvey.co.uk/s/CFR_feedback/?pageurl=';
+
 const formatCaseNumber = (caseNumber: string): string => {
   if (!caseNumber) {
     return '';
   }
   return caseNumber.replace(/(\d{4})(?=\d)/g, '$1-');
+};
+
+const getHeaderValue = (header: string | string[] | undefined): string | undefined => {
+  const value = Array.isArray(header) ? header[0] : header;
+
+  return value?.split(',')[0].trim();
+};
+
+const getCurrentUrl = (req: express.Request): URL => {
+  const forwardedProtocol = getHeaderValue(req.headers['x-forwarded-proto']);
+  const forwardedHost = getHeaderValue(req.headers['x-forwarded-host']);
+  const protocol = forwardedProtocol || req.protocol || 'http';
+  const host = forwardedHost || req.get('host') || 'localhost:3100';
+
+  return new URL(req.originalUrl || req.url || '/', `${protocol}://${host}`);
+};
+
+export const buildFeedbackSurveyUrl = (req: express.Request): string =>
+  `${FEEDBACK_SURVEY_BASE_URL}${encodeURIComponent(getCurrentUrl(req).href)}`;
+
+export const addNunjucksLocals: express.RequestHandler = (req, res, next) => {
+  res.locals.pagePath = req.path;
+  res.locals.feedbackSurveyUrl = buildFeedbackSurveyUrl(req);
+  next();
 };
 
 export class Nunjucks {
@@ -36,9 +62,6 @@ export class Nunjucks {
     env.addFilter('taskListFormItems', taskListFormItems);
     env.addFilter('formatCaseNumber', formatCaseNumber);
 
-    app.use((req, res, next) => {
-      res.locals.pagePath = req.path;
-      next();
-    });
+    app.use(addNunjucksLocals);
   }
 }
