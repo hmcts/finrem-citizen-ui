b214a54ab15492c499b82bf98ad2580f3130d1cf
adamg-hmcts <159780146+adamg-hmcts@users.noreply.github.com>
2026-09-17 15:20:27 +0100

    [DFR-5575] Cookie banner and cookies page (#600)
    * Initial cookie banner solution based on NFD and Probate
    
    * Use HMCTS cookie manager
    
    * Corrections to cookies page
    
    * Add E2E for cookie banner
    
    * Refactor
    
    * Pass routes into nunjucks for use in templates
    
    * Limit routes passed into nunjucks templates
    
    * Tweak cookie banner E2Es for local storage failures
    
    * Update constant imports following changes in master

--- src/main/constants/index.ts
+++ src/main/constants/index.ts
@@ -4,6 +4,7 @@ export const PublicRoutes = {
   callbackUrl: '/oauth2/callback',
   csrfError: '/csrf-error',
   config: '/config',
+  cookies: '/cookies',
   favicon: '/favicon.ico',
   health: '/health',
   info: '/info',
@@ -35,6 +36,7 @@ export const RouteNames = {
 } as const;
 
 export const ViewNames = {
+  Cookies: 'cookies',
   Dashboard: 'dashboard',
   EnterCaseNumber: 'enter-case-number',
   EnterAccessCode: 'enter-access-code',
